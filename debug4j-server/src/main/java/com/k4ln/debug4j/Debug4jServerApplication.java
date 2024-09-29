38ada4a23fd4f880c1c923ca705b9a8f8ab87976
k4ln <435360369@qq.com>
2024-09-29 17:30:14 +0800

    [-]初始化

---
+++ debug4j-server/src/main/java/com/k4ln/debug4j/Debug4jServerApplication.java
@@ -0,0 +1,11 @@
+package com.k4ln.debug4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Debug4jServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Debug4jServerApplication.class, args);
+    }
+}
