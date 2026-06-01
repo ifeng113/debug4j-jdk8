2ab5cddc3d8ea45bc6758a6a0f0d79c4d61d893c
k4ln <435360369@qq.com>
2026-06-01 09:26:41 +0800

    fix(protocol): 修复任务详情响应命令类型枚举值
    - 修正了 ATTACH_RESP_TASK_DETAILS 的十六进制值从 0x095 到 0x0905
    - 确保命令类型枚举值格式的一致性

--- debug4j-common/src/main/java/com/k4ln/debug4j/common/protocol/command/CommandTypeEnum.java
+++ debug4j-common/src/main/java/com/k4ln/debug4j/common/protocol/command/CommandTypeEnum.java
@@ -30,7 +30,7 @@ public enum CommandTypeEnum {
     ATTACH_RESP_CLASS_SOURCE("0x0902", "响应类源码"),
     ATTACH_RESP_CLASS_SOURCE_LINE("0x0903", "响应类源码"),
     ATTACH_RESP_TASK("0x0904", "响应任务列表"),
-    ATTACH_RESP_TASK_DETAILS("0x095", "响应任务详情"),
+    ATTACH_RESP_TASK_DETAILS("0x0905", "响应任务详情"),
 
     ;
 
